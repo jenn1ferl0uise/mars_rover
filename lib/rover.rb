e86f78b67562165adbf05b685504796876cc45af
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-11 12:42:31 +0200

    added helper method move_forward to rover, started helper method for nasa input

--- lib/rover.rb
+++ lib/rover.rb
@@ -4,6 +4,34 @@ class Rover
         @x_coord = attributes[:x_coord]
         @y_coord = attributes[:y_coord]
         @direction = attributes[:direction]
+        @plateau = Plateau.new
     end
+
+    def move_forward
+        case @direction
+        when 'N'
+            @y_coord += 1 if @y_coord < @plateau.y_max
+        when 'S'
+            @y_coord -= 1 if @y_coord < @plateau.y_min
+        when 'E'
+            @x_coord += 1 if @x_coord < @plateau.x_max
+        when 'W'
+            @x_coord -= 1 if @x_coord < @plateau.x_min
+        end
+    end
+
+    def turn_left
+    end
+
+    def turn_right
+    end
+
+    def nasa_input(string)
+        string.each_char do |input|
+            move_forward if input == 'M'
+            turn_left if input == 'L'
+            turn_right if input == 'R'
+        end
+    end 
 end
    

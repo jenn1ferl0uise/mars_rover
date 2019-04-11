1d83f1317d888055d67968998ad3ea489a4016dd
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-11 13:11:18 +0200

    wrote out nasa_controller test assumptions

---
+++ spec/nasa_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'nasa_controller'
+
+RSpec.describe NasaController do
+    let(:nasa_controller) { NasaController.new() }
+    it 'gives a starting string of the max coordinates for the plateau' do
+    end
+    it 'gives a string that corresponds to a rovers position on the plateau' do
+    end
+    it 'gives string of instructions to the rover L, R & M' do
+    end
+    it 'does not move another rover before finishing to move the first rover' do
+    end
+    it 'recieves an output string from the rover when it stops moving' do
+    end
+ end
+ 

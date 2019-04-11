1efc3ffd4dd37fe19c784cf4db45fc1198312c87
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-11 19:04:30 +0200

    preliminary finished app

--- lib/router.rb
+++ lib/router.rb
@@ -1,6 +1,15 @@
 class Router
   def initialize(nasa_controller)
     @nasa_controller = nasa_controller
-    @running = true
+  end
+
+  def run
+    @nasa_controller.interpret_nasa_information
+    @nasa_controller.set_plateau_limit
+    until @nasa_controller.nasa_input.nil?
+      @nasa_controller.select_rover
+      @nasa_controller.nasa_instructions
+      @nasa_controller.rover_end
+    end
   end
 end

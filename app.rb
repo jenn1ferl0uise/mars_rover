e5e06c62628b8cf27c375b576fc6ac00b107224e
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-11 18:56:45 +0200

    fixed rubocop errors, ready to start router methods

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require_relative 'lib/nasa_controller'
+require_relative 'lib/router'
 
 test_file = 'nasa_input.txt'
 
 nasa_controller = NasaController.new(nasa_input: test_file)
 
 router = Router.new(nasa_controller)
-
 
 router.run

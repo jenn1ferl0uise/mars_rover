6ff272d86101f40b9bbd995bfcdcb18a40e35d20
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-11 12:24:24 +0200

    refactor plateau testing

--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,20 +1,17 @@
 require 'plateau'
 
 RSpec.describe Plateau do
+    let(:plateau) { Plateau.new(x_max:5, y_max:5, x_min:0, y_min:0) }
     it 'has a maximum x coordinate' do
-        plateau = Plateau.new(x_max:'5')
-        expect(plateau.x_max).to eq('5')
+        expect(plateau.x_max).to eq(5)
     end
     it 'has a maximum y coordinate' do
-        plateau = Plateau.new(y_max: '5')
-        expect(plateau.y_max).to eq('5')
+        expect(plateau.y_max).to eq(5)
     end
     it 'has a minumum x coordinate' do
-        plateau = Plateau.new(x_min:0)
         expect(plateau.x_min).to eq(0)
     end
     it 'has a minimum y coordinate' do
-        plateau = Plateau.new(y_min: 0)
         expect(plateau.y_min).to eq(0)
     end
  end
